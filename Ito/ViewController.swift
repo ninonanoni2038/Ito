1e68fb88047271fd7fb12aacc4541776049ac6ef
gracia-experience <ninonanoni2038@gmail.com>
2020-04-03 18:28:22 +0900

    Initial Commit

---
+++ Ito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ito
+//
+//  Created by 二宮啓 on 2020/04/03.
+//  Copyright © 2020 二宮啓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ItoTests/ItoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ItoTests.swift
+//  ItoTests
+//
+//  Created by 二宮啓 on 2020/04/03.
+//  Copyright © 2020 二宮啓. All rights reserved.
+//
+
+import XCTest
+@testable import Ito
+
+class ItoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
