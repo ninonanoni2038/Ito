acdddd2a1c381c534f63df0521abcda3d712b932
Nino <ninonanoni2038@gmail.com>
2020-04-09 19:43:32 +0900

    add README.md

--- Ito/ViewController.swift
+++ Ito/ViewController.swift
@@ -6,20 +6,6 @@
 //  Copyright © 2020 二宮啓. All rights reserved.
 //
 
-//↓↓参考にした記事たち↓↓
-//https://www.cloverfield.co.jp/2018/05/08/mac-5/（Realmに画像保存）
-//https://qiita.com/saku/items/29c7fa20a62cc2f366fa（画像丸く表示）
-//https://yuu.1000quu.com/use_ui_tap_gesture_recognizer（Viewタップ時のイベント）
-//https://qiita.com/fummicc1-sub/items/a30e3cbfbf1148b0ec84（modalのDismiss時のイベント）
-//https://qiita.com/wadaaaan/items/d75b67ef712d49b2399e（日付の差分）
-//https://qiita.com/tojo/items/3189867605d3b1c5816e（通知）
-//https://capibara1969.com/1391/（NavBarButton）
-//https://qiita.com/fromage-blanc/items/d32642c467599a171f2c（addTargetでStringの値渡し）
-//https://qiita.com/Tsh-43879562/items/4883c433bb7297019a1f（画像のリサイズ）
-//https://program-life.com/413（メインスレッドでの画面遷移　紫のエラー）
-//https://terakoya.site/bb/ios-warning-attempt-present/（dismiss後にpresentしようとすると適切に表示されない）
-//http://www.finets.net/2018/08/04/your-app-uses-the-prefsroot-non-public-url-scheme/（Your app uses the “prefs:root=” non-public URL schemeリジェクトについて）
-//https://qiita.com/shtnkgm/items/c985ab4b314b8572eccd（final classについて）
 import UIKit
 import RealmSwift
 import UserNotifications
